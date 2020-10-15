61287730abc855c7950f92daa4a40c88356f51eb
Leo Vivier <zaeph@zaeph.net>
2020-10-15 19:49:14 +0200

    Squashed 'qa-lang/fr/' content from commit 045eea1
    git-subtree-dir: qa-lang/fr
    git-subtree-split: 045eea19d709bc9f3f33c13fa504bea5ccd71782

---
+++ qa-lang-users.php
@@ -0,0 +1,155 @@
+<?php
+	
+/*
+	Question2Answer (c) Gideon Greenspan
+
+	http://www.question2answer.org/
+
+	
+	File: qa-include/qa-lang-users.php
+	Version: See define()s at top of qa-include/qa-base.php
+	Description: Language phrases for user management
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+#     Traduction fr par Olivier SEREE (contrib[at]seree.fr) http://*.solu.fr
+#     	ajouts pour la 1.7 par Pierre-Yves Aillet (pierreyves.aillet@free.fr)
+
+	return array(
+		'about' => 'A propos',
+		'add_user_x_favorites' => 'Ajouter l\'utilisateur ^ à mes favoris',
+		'approve_required' => 'Patientez le temps que votre compte soit approuvé ou ^1ajoutez plus d\'information^2.',
+		'approve_title' => 'approbation de l\'utilisateur en cours',
+		'approve_user_button' => 'Approuver l\'utilisateur',
+		'approved_user' => 'Approuver l\'utilisateur',
+		'avatar_default' => 'Défaut',
+		'avatar_gravatar' => 'Afficher mon ^1Gravatar^2',
+		'avatar_label' => 'Avatar',
+		'avatar_none' => 'Aucun',
+		'block_user_button' => 'Bloquer l\'utilisateur',
+		'blocked_users' => 'Utilisateurs bloqués',
+		'category_level_add' => ' - ^1ajouter des privilèges spécifiques à la catégorie^2',
+		'category_level_in' => 'pour la catégorie ci-dessous',
+		'category_level_label' => 'Notifications vers',
+		'category_level_none' => 'Pas de mise a jour',
+		'change_email_link' => ' - ^1changer l\'email^2',
+		'change_password' => 'Changer le mot de passe',
+		'confirm_button' => 'Confirmer l\'adresse email',
+		'confirm_complete' => 'Merci - votre adresse électronique a été confirmée',
+		'confirm_emailed' => 'Un lien de confirmation a été envoyé par courriel. Merci de cliquer sur le lien pour confirmer votre adresse e-mail.',
+		'confirm_required' => 'Pour compléter votre inscription, cliquez sur le lien de confirmation qui vous a été envoyé par courriel, ou ^1demandez en un autre^2.',
+		'confirm_title' => 'Adresse email de confirmation',
+		'confirm_wrong_log_in' => 'Code incorrect - ^1Connectez vous^2 pour envoyer un nouveau lien',
+		'confirm_wrong_resend' => 'Code incorrect - cliquez ci-dessous pour envoyer un nouveau lien',
+		'delete_user_button' => 'Supprimer l\'utilisateur',
+		'edit_profile' => 'Modifier mon profil',
+		'edit_user_button' => 'Modifier l\'utilisateur',
+		'email_code_another' => 'envoyer un autre',
+		'email_code_emailed' => 'Votre code vous a été envoyé par mail',
+		'email_code_label' => 'Code :',
+		'email_code_wrong' => 'Code incorrect',
+		'email_confirmed' => 'Confirmé',
+		'email_exists' => 'Cet email appartient déjà à un compte',
+		'email_handle_label' => 'Email ou Pseudo',
+		'email_invalid' => 'Le courriel est invalide - Veuillez le vérifier',
+		'email_label' => 'Email',
+		'email_not_confirmed' => 'Pas encore confirmé',
+		'email_please_confirm' => 'S\'il vous plait ^5confirmez^6',
+		'email_required' => 'Adresse e-mail requis - non publique',
+		'forgot_link' => 'J\'ai oublié mon mot de passe',
+		'full_name' => 'Prénom et nom',
+		'handle_blocked' => 'Nom d\utilisateur interdit - merci d\'en essayer un autre',
+		'handle_empty' => 'Le nom d\'utilisateur ne doit pas être vide',
+		'handle_exists' => 'Ce nom d\'utilisateur est pris -  essayer en un autre',
+		'handle_has_bad' => 'Le nom d\'utilisateur ne doit pas contenir ^',
+		'handle_label' => 'Pseudo',
+		'hide_all_user_button' => 'Masquer tous les messages de cet utilisateur',
+		'last_login_label' => 'Dernière connexion',
+		'last_write_label' => 'Dernière action d\'écriture',
+		'level_admin' => 'Administrateur',
+		'level_editor' => 'Editeur',
+		'level_expert' => 'Expert',
+		'level_for_category' => '^1 pour ^2',
+		'level_in_general' => 'en general',
+		'level_moderator' => 'Moderateur',
+		'level_super' => 'Super Administrateur',
+		'location' => 'Lieu',
+		'log_in_to_access' => 'Vous pouvez maintenant ^1vous connectez^2 accéder à votre compte.',
+		'login_button' => 'Connexion',
+		'login_limit' => 'Trop de tentatives de connexion - Veuillez réessayer dans une heure',
+		'login_title' => 'Connexion',
+		'mass_mailings' => 'Mailings de masse',
+		'mass_mailings_explanation' => 'Abonnez-vous aux e-mails envoyés à tous les utilisateurs',
+		'member_for' => 'Membre depuis',
+		'member_type' => 'Fonction',
+		'new_password_1' => 'Nouveau mot de passe',
+		'new_password_2' => 'Retaper le nouveau mot de passe',
+		'no_blocked_users' => 'Aucun utilisateur bloqué trouvé',
+		'no_permission' => 'Vous n\'avez pas la permission d\'effectuer cette opération',
+		'old_password' => 'Ancien mot de passe',
+		'only_shown_admins' => '(seulement visibles par les administrateurs)',
+		'only_shown_editors' => '(seulement visibles par les éditeurs et au-dessus)',
+		'only_shown_experts' => '(seulement visibles par les experts et au-dessus)',
+		'only_shown_moderators' => '(seulement visibles par les modérateurs et administrateurs)',
+		'password_changed' => 'Mot de passe modifié',
+		'password_label' => 'Mot de passe',
+		'password_min' => 'Le mot de passe doit comporter au moins  ^ caractères',
+		'password_mismatch' => 'Les nouveaux mots de passe ne correspondent pas',
+		'password_none' => 'Aucun mot de passe. Pour se connecter directement , définissez un mot de passe ci-dessous.',
+		'password_sent' => 'Votre nouveau mot de passe vous a été envoyé par courriel',
+		'password_to_set' => 'Mot de passe indéfini',
+		'password_wrong' => 'Mot de passe incorrect',
+		'private_messages' => 'Messages Privés',
+		'private_messages_explanation' => 'Autoriser les utilisateurs à vous envoyer un mail (sans voir votre adresse)',
+		'profile_saved' => 'Profil sauvé',
+		'register_button' => 'Inscription',
+		'register_limit' => 'Trop d\'inscriptions - essayez à nouveau dans une heure',
+		'register_suspended' => 'L\'inscription de nouveaux utilisateurs a été temporairement suspendu .',
+		'register_title' => 'Enregistrement d\'un nouvel utilisateur',
+		'registered_label' => 'Enregistré',
+		'registered_user' => 'Utilisateur enregistré',
+		'remember' => 'Souvenir',
+		'remember_label' => 'Se souvenir de moi sur cet ordinateur',
+		'remove_avatar' => 'Supprimer avatar',
+		'reset_code_another' => 'envoyer un autre',
+		'reset_code_emailed' => 'votre code de réinitialisation vous a été envoyé',
+		'reset_code_label' => 'Code',
+		'reset_code_wrong' => 'Code incorrect',
+		'reset_title' => 'Reset Mot de passe oublié',
+		'save_profile' => 'Enregistrer le profil',
+		'save_user' => 'Enregistrer l\'utilisateur',
+		'send_confirm_button' => 'Envoyer le lien de confirmation',
+		'send_password_button' => ' Envoyer un nouveau mot de passe ',
+		'send_reset_button' => 'Envoyer nouveau mot de passe par e-mail',
+		'send_reset_note' => 'Un message sera envoyé à votre adresse e-mail avec les instructions.',
+		'special_users' => 'Utilisateurs spéciaux',
+		'terms_not_accepted' => 'Vous devez cocher la case.',
+		'unblock_user_button' => 'Débloquer l\'utilisateur ',
+		'unsubscribe' => 'Se désabonner',
+		'unsubscribe_complete' => 'Vous vous êtes désabonné des publipostages envoyés par ^0. Vous pouvez vous réinscrire à tout moment via votre ^1compte^2.', 
+		'unsubscribe_title' => 'Désabonner',
+		'unsubscribe_wrong_log_in' => 'Code incorrect - ^1Connectez vous^2 pour vous désabonner',
+		'user_blocked' => '(bloqué)',
+		'user_not_found' => 'Utilisateur introuvable',
+		'wall_posts' => 'Messages du mur',
+		'wall_posts_explanation' => 'Autoriser les utilisateurs à publier sur votre mur (vous aurez également les messages par courriel)',
+		'website' => 'Site web',
+		'x_ago_from_y' => 'il y a ^1 par ^2',
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
